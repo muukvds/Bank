f909dee1d98c774a27eb71ed76db5cc3ee77ea15
muukvds <muukvds@users.noreply.github.com>
2017-09-27 13:19:37 +0200

    Call methods in Main class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args)
     {
         BankAccount bankAccount = new BankAccount(123456,"Bob",200,1234);
+        bankAccount.deposit(800);
+
+        bankAccount.withDraw(0231,200);
+        bankAccount.withDraw(1234,200);
+        bankAccount.withDraw(1234,-200);
     }
 
 }
