0598ed93971180479746c9e77044e01bc46118fd
muukvds <muukvds@users.noreply.github.com>
2017-09-27 12:52:13 +0200

    create project, create BankAccount class

---
+++ src/BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount {
+    private int accountNumber;
+    private String ownerName;
+    private int creditLevel;
+    private int securityCode;
+
+
+    public void withDraw(int code, int amount) {
+
+    }
+
+    public void deposit(int amount) {
+
+    }
+}
